0bae40b710f2f1ec4edb3957f570dbbf8b021bc5
Truong Nguyen <truong36gm@gmail.com>
2025-04-04 19:59:40 -0400

    add try, except

--- png2txt.py
+++ png2txt.py
@@ -4,11 +4,14 @@ import pytesseract
 
 fname = 'test'
 
-input_png = f"{fname}.png"         
-text = str(pytesseract.image_to_string(Image.open(input_png)))
-if len(text):
+input_png = f"{fname}.png"       
+try:  
+    text = str(pytesseract.image_to_string(Image.open(input_png)))
+
     output_txt = f'{fname}.txt' #where you want to save and file name
     fw = open(output_txt, "a")
 
     fw.write(text)
     fw.close()
+except: 
+    print("Some error")
